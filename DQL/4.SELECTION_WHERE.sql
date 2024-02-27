0c87ae27161b1ec2bc9e35aceb3679437ee4d368
Nishkala Harikantra <nishkalaharikantra25@gmail.com>
2024-02-27 15:32:40 +0530

    removed extra space

--- DQL/4.SELECTION_WHERE.sql
+++ DQL/4.SELECTION_WHERE.sql
@@ -1,10 +1,10 @@
 --WAQTD details of Nayan.
-	select *
-	from emp
-	where ename = "Nayan";
+select *
+from emp
+where ename = "Nayan";
 
 --WAQTD names of the emp if the emp working as manager.
-	select ename
-	from emp
-	where job = "Manager";
+select ename
+from emp
+where job = "Manager";
 
